1389503b9a6e4bc89c8646270a9e26c7c96da1b9
Amir Abdel Rahman <meero@Amirs-MacBook-Pro.local>
2023-05-12 02:04:31 -0400

    used date-fns library to try and fix coutndown browser compatibility

--- src/components/CountDown.js
+++ src/components/CountDown.js
@@ -1,43 +1,41 @@
 import React, { useState, useEffect } from 'react';
+import { differenceInSeconds, addSeconds } from 'date-fns';
 
 const CountdownTimer = () => {
-    const getRemainingTime = (endTime) => {
-        const currentTime = new Date();
-        const difference = endTime - currentTime;
-        const remainingDays = Math.floor(difference / 1000 / 60 / 60 / 24);
-        const remainingHours = Math.floor((difference / 1000 / 60 / 60) % 24);
-        const remainingMinutes = Math.floor((difference / 1000 / 60) % 60);
-        const remainingSeconds = Math.floor((difference / 1000) % 60);
-        return { days: remainingDays, hours: remainingHours, minutes: remainingMinutes, seconds: remainingSeconds };
-      };
-      
-    const fetchDuration = async () => {
-    const response = await fetch('http://localhost:3001/countdown-duration');
-    const data = await response.json();
-    return data.duration;
-    };
-    
+  const getRemainingTime = (endTime) => {
+    const currentTime = new Date();
+    const totalSeconds = differenceInSeconds(endTime, currentTime);
+    const remainingDays = Math.floor(totalSeconds / 60 / 60 / 24);
+    const remainingHours = Math.floor((totalSeconds / 60 / 60) % 24);
+    const remainingMinutes = Math.floor((totalSeconds / 60) % 60);
+    const remainingSeconds = Math.floor(totalSeconds % 60);
+    return { days: remainingDays, hours: remainingHours, minutes: remainingMinutes, seconds: remainingSeconds };
+  };
 
-  const [remainingTime, setRemainingTime] = useState({ hours: 0, minutes: 0, seconds: 0 });
+  const fetchEndTime = async () => {
+    const response = await fetch('http://localhost:3001/countdown-end-time');
+    const data = await response.json();
+    return addSeconds(new Date(), data.endTime); // we use `addSeconds` from `date-fns` here to parse the timestamp
+  };
+
+  const [remainingTime, setRemainingTime] = useState({ days: 0, hours: 0, minutes: 0, seconds: 0 });
 
   useEffect(() => {
-    let interval;
     const updateRemainingTime = async () => {
-      const duration = await fetchDuration();
-      const endTime = new Date().getTime() + duration;
+      const endTime = await fetchEndTime();
+      setRemainingTime(getRemainingTime(endTime));
 
-      interval = setInterval(() => {
-        setRemainingTime(getRemainingTime(new Date(endTime)));
+      const interval = setInterval(() => {
+        setRemainingTime(getRemainingTime(endTime));
       }, 1000);
+
+      return () => {
+        clearInterval(interval);
+      };
     };
 
     updateRemainingTime();
-
-    return () => {
-      clearInterval(interval);
-    };
   }, []);
-
 
   const { days: d, hours: h, minutes: m, seconds: s } = remainingTime;
   return (
@@ -45,11 +43,9 @@ const CountdownTimer = () => {
       {d} {d === 1 ? 'day' : 'days'} {h} {h === 1 ? 'hour' : 'hours'} {m} {m === 1 ? 'minute' : 'minutes'} {s} {s === 1 ? 'second' : 'seconds'}
     </div>
   );
-  
-
-  
 };
 
 export default CountdownTimer;
+
 
 
