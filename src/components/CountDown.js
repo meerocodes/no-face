9c76242f3d37b5487510dd821441ff482d223cd3
Amir Abdel Rahman <meero@Amirs-MacBook-Pro.local>
2023-05-12 02:10:46 -0400

    reverted code back as countdown does not work on any browsers ATM

--- src/components/CountDown.js
+++ src/components/CountDown.js
@@ -2,39 +2,42 @@ import React, { useState, useEffect } from 'react';
 import { differenceInSeconds, addSeconds } from 'date-fns';
 
 const CountdownTimer = () => {
-  const getRemainingTime = (endTime) => {
-    const currentTime = new Date();
-    const totalSeconds = differenceInSeconds(endTime, currentTime);
-    const remainingDays = Math.floor(totalSeconds / 60 / 60 / 24);
-    const remainingHours = Math.floor((totalSeconds / 60 / 60) % 24);
-    const remainingMinutes = Math.floor((totalSeconds / 60) % 60);
-    const remainingSeconds = Math.floor(totalSeconds % 60);
+const getRemainingTime = (endTime) => {
+    const currentTime = new Date().getTime(); // gets current time in milliseconds
+    const difference = endTime - currentTime;
+    
+    const remainingDays = Math.floor(difference / (1000 * 60 * 60 * 24));
+    const remainingHours = Math.floor((difference / (1000 * 60 * 60)) % 24);
+    const remainingMinutes = Math.floor((difference / (1000 * 60)) % 60);
+    const remainingSeconds = Math.floor((difference / 1000) % 60);
+    
     return { days: remainingDays, hours: remainingHours, minutes: remainingMinutes, seconds: remainingSeconds };
-  };
+    };
+      
 
   const fetchEndTime = async () => {
     const response = await fetch('http://localhost:3001/countdown-end-time');
     const data = await response.json();
-    return addSeconds(new Date(), data.endTime); // we use `addSeconds` from `date-fns` here to parse the timestamp
+    return data.endTime; // use the timestamp directly
   };
 
   const [remainingTime, setRemainingTime] = useState({ days: 0, hours: 0, minutes: 0, seconds: 0 });
 
   useEffect(() => {
+    let interval;
     const updateRemainingTime = async () => {
       const endTime = await fetchEndTime();
-      setRemainingTime(getRemainingTime(endTime));
-
-      const interval = setInterval(() => {
+      
+      interval = setInterval(() => {
         setRemainingTime(getRemainingTime(endTime));
       }, 1000);
-
-      return () => {
-        clearInterval(interval);
-      };
     };
 
     updateRemainingTime();
+
+    return () => {
+      clearInterval(interval);
+    };
   }, []);
 
   const { days: d, hours: h, minutes: m, seconds: s } = remainingTime;
