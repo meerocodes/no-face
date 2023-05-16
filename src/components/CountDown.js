a1d34fe8025883c675bd55d56b228c1ce5fe22ad
Amir Abdel Rahman <meero@Amirs-MacBook-Pro.local>
2023-05-15 20:55:42 -0400

    Added a server and made some changes

--- src/components/CountDown.js
+++ src/components/CountDown.js
@@ -1,31 +1,28 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Countdown from 'react-countdown';
 
 // Renderer callback with condition
 const renderer = ({ days, hours, minutes, seconds, completed }) => {
   if (completed) {
+    // Render a completed state
     return <span>You are good to go!</span>;
   } else {
-    return <span>{days} days, {hours} hours, {minutes} minutes, {seconds} seconds remaining</span>;
+    // Render a countdown
+    return <span className='countDown'>{days} days, {hours} hours, {minutes} minutes, {seconds} seconds remaining</span>;
   }
 };
 
 export default function CountdownTimer() {
-  const [endDate, setEndDate] = useState(null);
+  // Calculate the time 10 days from now
+  const tenDaysFromNow = Date.now() + 10 * 24 * 60 * 60 * 1000;
 
-  useEffect(() => {
-    fetch('/api/countdown')
-      .then(response => response.json())
-      .then(data => setEndDate(data.countdownEnd));
-  }, []);
-
-  if (endDate === null) {
-    return <div>Loading...</div>;
-  }
-
-  return <Countdown date={endDate} renderer={renderer} />;
+  return (
+    <Countdown
+      date={tenDaysFromNow}
+      renderer={renderer}
+    />
+  );
 }
-
 
 
 
