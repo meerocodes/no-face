833ca69f24340838d15728b31f923a9e44b9cb00
Amir Abdel Rahman <meero@Amirs-MacBook-Pro.local>
2023-05-15 20:45:04 -0400

    added server side to maintain countdown integrity accross all devices

--- src/components/CountDown.js
+++ src/components/CountDown.js
@@ -1,28 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Countdown from 'react-countdown';
 
 // Renderer callback with condition
 const renderer = ({ days, hours, minutes, seconds, completed }) => {
   if (completed) {
-    // Render a completed state
     return <span>You are good to go!</span>;
   } else {
-    // Render a countdown
-    return <span className='countDown'>{days} days, {hours} hours, {minutes} minutes, {seconds} seconds remaining</span>;
+    return <span>{days} days, {hours} hours, {minutes} minutes, {seconds} seconds remaining</span>;
   }
 };
 
 export default function CountdownTimer() {
-  // Calculate the time 10 days from now
-  const tenDaysFromNow = Date.now() + 10 * 24 * 60 * 60 * 1000;
+  const [endDate, setEndDate] = useState(null);
 
-  return (
-    <Countdown
-      date={tenDaysFromNow}
-      renderer={renderer}
-    />
-  );
+  useEffect(() => {
+    fetch('/api/countdown')
+      .then(response => response.json())
+      .then(data => setEndDate(data.countdownEnd));
+  }, []);
+
+  if (endDate === null) {
+    return <div>Loading...</div>;
+  }
+
+  return <Countdown date={endDate} renderer={renderer} />;
 }
+
 
 
 
