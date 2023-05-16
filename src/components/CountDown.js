fe6e9f66dfc2ffd1d202d9a069fdd42e687ac41c
Amir Abdel Rahman <meero@Amirs-MacBook-Pro.local>
2023-05-15 21:03:30 -0400

    further edits to countdown component

--- src/components/CountDown.js
+++ src/components/CountDown.js
@@ -1,28 +1,41 @@
-import React from 'react';
-import Countdown from 'react-countdown';
+// Countdown.js
 
-// Renderer callback with condition
-const renderer = ({ days, hours, minutes, seconds, completed }) => {
-  if (completed) {
-    // Render a completed state
-    return <span>You are good to go!</span>;
-  } else {
-    // Render a countdown
-    return <span className='countDown'>{days} days, {hours} hours, {minutes} minutes, {seconds} seconds remaining</span>;
-  }
+import React, { useEffect, useState } from "react";
+import useCountdown from "react-countdown-hook";
+
+const Countdown = () => {
+  const [timeLeft, { start }] = useCountdown(0, 1000); // Initialize with 0
+  const [hasFetchedTime, setHasFetchedTime] = useState(false);
+
+  useEffect(() => {
+    if (!hasFetchedTime) {
+      fetch("http://localhost:3001/endtime")
+        .then((res) => res.json())
+        .then((data) => {
+          const now = new Date();
+          const endTime = new Date(data.endTime);
+          const timeToCountDown = endTime - now;
+
+          if (timeToCountDown > 0) {
+            start(timeToCountDown);
+          }
+
+          setHasFetchedTime(true);
+        });
+    }
+  }, [hasFetchedTime, start]);
+
+  const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+  return <div className="countDown">{`${days}d ${hours}h ${minutes}m ${seconds}s`}</div>;
 };
 
-export default function CountdownTimer() {
-  // Calculate the time 10 days from now
-  const tenDaysFromNow = Date.now() + 10 * 24 * 60 * 60 * 1000;
+export default Countdown;
 
-  return (
-    <Countdown
-      date={tenDaysFromNow}
-      renderer={renderer}
-    />
-  );
-}
+
 
 
 
