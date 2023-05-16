db5202fea7a60d7b78c75fd61f30e36bf89c86b0
Amir Abdel Rahman <meero@Amirs-MacBook-Pro.local>
2023-05-15 20:28:46 -0400

    attempting countdown with a new npm library

--- src/components/CountDown.js
+++ src/components/CountDown.js
@@ -1,43 +1,14 @@
-import React, { useEffect } from 'react';
-import useCountDown from 'react-countdown-hook';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Countdown from 'react-countdown';
 
-const CountdownTimer = () => {
-  const fetchEndTime = async () => {
-    const response = await fetch('http://localhost:3001/countdown-end-time');
-    const data = await response.json();
-    return data.endTime; // use the timestamp directly
-  };
+// Calculate the date 10 days from now
+const dateInTenDays = Date.now() + 10 * 24 * 60 * 60 * 1000;
 
-  const initialTime = 0; // initial time in milliseconds, it will be updated after fetching end time
-  const interval = 1000; // interval to change remaining time amount, defaults to 1000
-
-  const [timeLeft, { start }] = useCountDown(initialTime, interval);
-
-  useEffect(() => {
-    const setEndTime = async () => {
-      const endTime = await fetchEndTime();
-      const currentTime = new Date().getTime();
-      const remainingTime = endTime - currentTime;
-      start(remainingTime);
-    };
-
-    setEndTime();
-  }, []);
-
-  const d = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-  const h = Math.floor((timeLeft / (1000 * 60 * 60)) % 24);
-  const m = Math.floor((timeLeft / (1000 * 60)) % 60);
-  const s = Math.floor((timeLeft / 1000) % 60);
-
-  return (
-    <div className='countDown'>
-      {d} {d === 1 ? 'day' : 'days'} {h} {h === 1 ? 'hour' : 'hours'} {m} {m === 1 ? 'minute' : 'minutes'} {s} {s === 1 ? 'second' : 'seconds'}
-    </div>
-  );
-};
-
-export default CountdownTimer;
-
+ReactDOM.render(
+  <Countdown date={dateInTenDays} />,
+  document.getElementById('root')
+);
 
 
 
