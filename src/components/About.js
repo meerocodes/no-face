d6a34c9557c26ae53bf55576ccc981ed83d4d968
Amir Abdel Rahman <meero@Amirs-MacBook-Pro.local>
2023-06-27 18:50:31 -0400

    Added and styled about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,29 @@
 import { Link } from "react-router-dom";
+import logo3d from '../assets/noFaceLogoBackgroundless.gif'
+
 
 const About = () => {
     return (
-        <div className="About">
+        <div className="aboutPage">
             <div className="wrapper">
-                <p>About nfc </p>
-                <Link to='/'><button class="back"> Home</button></Link>
+                <div className="topAbout">
+                    <h1>WHO IS NOFACECLUB? </h1>
+                    <h2><span>F</span>ounded in November 2019, <span>NOFACECLUB</span> is a collective of visionary minds driven by a singular purpose: to stand the f*ck out. We
+                    craft high-quality garments that encapsulate culture, history, and contemporary street-life.</h2>
+                </div>
+     
+          
+
+                <div className="noFaceIsUs">
+                    <h3>Witness what we make.</h3>
+                    <h3>Monthly drops.</h3>
+                    <h3>Launching Summer 2023.</h3>
+                </div>
+
+                <div className="bottomAbout">
+                    <h1>LOOK <br></br>DIFFERENT</h1>
+                <Link to='/preOrder'><img src={logo3d} alt ='no face logo 3d render gif'></img></Link>
+                </div>
 
             </div>
         </div>
