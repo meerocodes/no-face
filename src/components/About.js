ffbdff03e128312e148313fef9506a64ffc39d75
Amir Abdel Rahman <meero@Amirs-MacBook-Pro.local>
2023-07-08 19:15:26 -0400

    added minor details in styling with client

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ const About = () => {
             <div className="wrapper">
                 <div className="topAbout">
                 <Link to='/preOrder'><img src={logo3d}  className='aboutLogo' alt ='no face logo 3d render gif'></img></Link>
-                    <h2><span>F</span>ounded in November 2019, <span>NOFACECLUB</span> is a collective of visionary minds driven by a singular purpose: to stand the f*ck out. We
+                    <h2><span id='founded'>F</span>ounded in November 2019, <span>NOFACECLUB </span>  is a collective of visionary minds driven by a singular purpose: to stand the f*ck out. We
                     craft high-quality garments that encapsulate culture, history, and contemporary street-life.</h2>
                 </div>
      
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -13,7 +13,7 @@ const Nav = () => {
                 <div className="wrapper">
                     <h1>NOFACECLUB</h1>
                     <Link to='/'><img src={logo} alt="Logo" /></Link>
-                    <Link to ='/About'> <h3 className="about">ABOUT</h3></Link>
+                     <h3 className="about"><Link to ='/About'>ABOUT  </Link></h3>
                 </div>
             </nav>
         </header>
